cccb726e18ce2183e22a35057ddb968b196f64f9
shivam1107verma <shivam1107verma@gmail.com>
2023-05-29 02:08:12 +0530

    again commit

---
+++ JavaScript/Popups/script.js
@@ -0,0 +1,17 @@
+alert("Hellow World!");
+
+var age = prompt("Enter your age", 18);
+console.log("age" + age);
+
+if(age >= 18){
+    console.log("user is eligible")
+}
+else{
+    console.log("User is not eligible")
+}
+
+var radius = parseInt(prompt("Radius"));
+console.log(radius);
+
+var area = 3.14 + radius + radius ;
+console.log("Area of Circle is :", area)
